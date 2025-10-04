6e3584a713c6b3c07e97cd77afafe8dd82cc3c12
soham gadhave <sohamgadhave527@gmail.com>
2025-10-04 19:51:38 +0530

    fourth commit

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/globe";
 import CopyEmailButton from "../components/CopyEmailButton";
-import { Frameworks } from "../components/Frameworks";
+import  Frameworks  from "../components/Frameworks";
 import { RoundedBox } from "@react-three/drei";
 
 const About = () => {
